9c14b61c60c62a37eb017052ffa09cf8f0be9b1e
lemon <lp951195660@163.com>
2021-07-14 14:25:33 +0800

    first commit

--- sparrow/endpoint/v1/user.py
+++ sparrow/endpoint/v1/user.py
@@ -15,14 +15,14 @@ from sparrow.core.logger import Logger
 router = Blueprint("user", __name__)
 
 
-@router.get("")
+@router.get("/")
 @Logger("用户管理", "获取用户详情")
 def get_user():
     user = {"username": "张三", "age": 12}
     return jsonify(user)
 
 
-@router.get("/list")
+@router.get("/list/")
 @Logger("用户管理", "获取用户列表")
 def get_user_list():
     users = [{"username": "张三", "age": 12}, {"username": "李四", "age": 22}]
